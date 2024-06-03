f66b2b74bc3390c55a16aa3d11ad87d73190a939
Md Irfan Mullick <mdirfan.mullick@publicissapient.com>
2024-06-03 14:14:09 +0530

    Add multiple transaction max profit calculation Leetcode Top Interview Q #8

--- src/MaxProfit.java
+++ src/MaxProfit.java
@@ -1,7 +1,8 @@
 public class MaxProfit {
 
     /**
-     * Returns the maximum profit that can be made from buying and selling stocks.
+     * Returns the maximum profit that can be made from buying and selling stocks in
+     * a single transaction.
      *
      * @param prices the stock prices
      * @return the maximum profit
@@ -11,6 +12,21 @@ public class MaxProfit {
         for (int i = 1; i < prices.length; i++) {
             maxProfit = Math.max(maxProfit, prices[i] - minPrice);
             minPrice = Math.min(minPrice, prices[i]);
+        }
+        return maxProfit;
+    }
+
+    /**
+     * Returns the maximum profit that can be made from buying and selling stocks in
+     * multiple transaction.
+     *
+     * @param prices the stock prices
+     * @return the maximum profit
+     */
+    static int maxProfit2(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            maxProfit += Math.max(0, prices[i] - prices[i - 1]);
         }
         return maxProfit;
     }
